6019547c1c7367988c9186ff42876a35c9aec46a
LLCHernandez <wowacct25@gmail.com>
2024-04-10 14:34:24 -0500

    fixed a few functional errors, saving to do a git pull origin main

--- assets/js/home.js
+++ assets/js/home.js
@@ -43,3 +43,8 @@ function getMovies() {
             // }
     })
 }
+
+if (window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.body.classList.add('dark');
+}
